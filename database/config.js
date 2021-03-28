bed3661f67582a1dde810cd37f38082344b01daf
xMichaelRodriguez <scottlovos503@gmail.com>
2021-03-27 18:29:39 -0600

    resolve

--- database/config.js
+++ database/config.js
@@ -1,28 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.db_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
 
-const dbConnection = async() => {
-
-    try {
-        
-        await mongoose.connect( process.env.DB_CNN , {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-
-        console.log('DB Online');
-
-
-    } catch (error) {
-        console.log(error);
-        throw new Error('Error a la hora de inicializar BD');
-    }
-
-
-}
-
+    console.log("DB Online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error a la hora de inicializar BD");
+  }
+};
 
 module.exports = {
-    dbConnection
-}
+  dbConnection,
+};
